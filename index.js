79c840b8a2894345f0e44d0119054cb21814c3fc
Quinn Michaels <quinn@indra.ai>
2025-09-12 10:29:23 -0700

    kshatriya upate deva core

--- index.js
+++ index.js
@@ -41,17 +41,6 @@ const KSHATRIYA = new Deva({
   devas: {},
   func: {},
   methods: {},
-  async onInit(data, resolve) {
-    // check license
-    const {personal} = this.license(); // get the license config
-    this.vars.license = await this.methods.license_check(personal, pkg.VLA);
-    if (this.vars.license) {
-      return this.start(data, resolve); // start load if license valid
-    }
-    else {       
-      return  this.stop(data, resolve); // stop load if license is invalid.
-    }
-  },
   onReady(data, resolve) {
     this.prompt(this.vars.messages.ready);
     return resolve(data);
